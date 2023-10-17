ff2e92358b3c6aa03f7535482dd8f3be6b0f1b23
Adithyan Anil <adithyananil2002@gmail.com>
2023-10-17 19:41:48 +0530

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar,Container,Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping,faHeart } from '@fortawesome/free-solid-svg-icons'
+export default function Header() {
+  return (
+    <>   <Navbar bg="primary" data-bs-theme="dark">
+    <Container>
+      <Link to={"/"} style={{textDecoration:'none',overflow:'hidden'}} ><Navbar.Brand >E-cart</Navbar.Brand></Link>
+      <Nav className=" d-flex gap-3">         
+     {/* <Nav.Link ><Link to={"/"} style={{textDecoration:'none',color:'white'}}> Home  </Link></Nav.Link> */}
+        <button className='btn ' ><Link to={"/wishlist"} style={{textDecoration:'none',color:'white'}}><FontAwesomeIcon icon={faHeart} size="lg" style={{color: "#ff0000",}} /> Wishlist (<span >10</span>) </Link></button>
+      <button className='btn' ><Link to={"/cart"} style={{textDecoration:'none',color:'white'}}> <FontAwesomeIcon icon={faCartShopping} size='lg'  style={{color: "white"}} /> <sup style={{backgroundColor:'red',padding:'1px'}} className='rounded'>10</sup>{""} Cart </Link></button>
+      </Nav>
+    </Container>
+  </Navbar></>
+  )
+}
